a926868363fb88a97b7212ac3bde03b36792ae39
Damien Le Thiec <damien.lethiec@gmail.com>
2017-10-24 17:10:25 -0300

    add first js

--- src/App/components/molecules/SearchForm/index.js
+++ src/App/components/molecules/SearchForm/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import styled from "styled-components";
 import Input from "./../../atoms/Input/index";
 import Button from "./../../atoms/Button/index.js";
@@ -8,6 +8,7 @@ const SearchFormBox = styled.div`
   justify-content: center;
   align-items: center;
   flex-direction: column;
+
   ${props =>
     props.inline &&
     `
@@ -15,11 +16,38 @@ const SearchFormBox = styled.div`
   `};
 `;
 
-export default function SearchForm(props) {
-  return (
-    <SearchFormBox inline={props.inline}>
-      <Input placeholder="St. George, Utah" />
-      <Button green>Get Weather</Button>
-    </SearchFormBox>
-  );
+class SearchForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      value: ""
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(event) {
+    var value = event.target.value;
+    this.setState(function() {
+      return {
+        value: value
+      };
+    });
+  }
+  render() {
+    return (
+      <SearchFormBox inline={this.props.inline}>
+        <Input
+          type="text"
+          autoComplete="off"
+          placeholder="St. George, Utah"
+          onChange={this.handleChange}
+          value={this.state.value}
+        />
+        <Button green>Get Weather</Button>
+      </SearchFormBox>
+    );
+  }
 }
+
+export default SearchForm;
---
+++ src/App/utilities/functions/WeatherApi.js
@@ -0,0 +1,7 @@
+import axios from "axios";
+import cities from "./cities.json";
+
+function getCurrentWeather(city) {
+  console.log(cities[0])
+}
+getCurrentWeather("test")
