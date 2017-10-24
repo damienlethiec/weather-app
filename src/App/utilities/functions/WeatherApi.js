090cf8193eb4f754a189bbb11a9f520c213c5813
Damien Le Thiec <damien.lethiec@gmail.com>
2017-10-24 17:10:44 -0300

    add first js 2

--- src/App/utilities/functions/WeatherApi.js
+++ src/App/utilities/functions/WeatherApi.js
@@ -2,6 +2,6 @@ import axios from "axios";
 import cities from "./cities.json";
 
 function getCurrentWeather(city) {
-  console.log(cities[0])
+  console.log(cities[0]);
 }
-getCurrentWeather("test")
+getCurrentWeather("test");
